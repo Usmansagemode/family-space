8bd8e7bef39552f9339e6f3683b48059203eff43
Usmansagemode <usmansagemode@gmail.com>
2026-03-05 01:01:37 -0500

    fixing npm install and vercel issue

--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
+    nitro({ preset: 'vercel' }),
     contentCollections(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    tanstackStart({
-      server: {
-        preset: 'vercel',
-      },
-    }),
+    tanstackStart(),
     viteReact(),
   ],
 })
