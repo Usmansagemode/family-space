556129e806eb35c4138fa8f87b426fda5c5261aa
Usmansagemode <usmansagemode@gmail.com>
2026-03-06 02:22:57 -0500

    vercel caching fix

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D2JbUt_L.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/Users/usmankhalid/Documents/Personal Projects/family-calendar/src/routes/__root.tsx", "children": ["/", "/invite"], "preloads": ["/assets/main-BL_wodfN.js"], "assets": [] }, "/": { "filePath": "/Users/usmankhalid/Documents/Personal Projects/family-calendar/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-CD3ibwZ6.js"] }, "/invite": { "filePath": "/Users/usmankhalid/Documents/Personal Projects/family-calendar/src/routes/invite.tsx", "assets": [], "preloads": ["/assets/invite-BaWdWnxA.js"] } }, "clientEntry": "/assets/main-BL_wodfN.js" });
-export {
-  tsrStartManifest
-};
